d0007076c17fedd9d6b338159754a78953097a26
reddigger <gheorghii.milovanov@gmail.com>
2018-05-02 22:12:13 +0300

    creare controalerelor Country si City

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    public function showCitiesList()
+    {
+    	echo 'Lista cu orase';
+    }
+
+    public function showAddForm()
+    {
+    	echo 'Lista cu orase';
+    }
+
+    public function addCity()
+    {
+    	echo 'adaugarea datelor prin post';
+    }
+}
---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    public function showCountriesList()
+    {
+    	echo 'Lista cu orase';
+    }
+
+    public function showAddForm()
+    {
+    	echo 'Lista cu orase';
+    }
+
+    public function addCoutry()
+    {
+    	echo 'adaugarea datelor prin post';
+    }
+}
